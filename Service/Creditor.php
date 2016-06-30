8a0d326939dd1017bd89ef3df9ba97f437be2612
Rune Mønnike <rm@gospoort.com>
2016-06-30 16:01:17 +0200

    Added basic services.

---
+++ Service/Creditor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of SPOORT International ApS
+ * and belongs to SPOORT International ApS.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Spoort\Bundle\Symfony2EconomicBundle\Service;
+
+use Spoort\Bundle\Symfony2EconomicBundle\Service\SoapApi as SoapApiService;
+
+
+class Creditor
+{
+    /** @var $soapApiService SoapApiService */
+    private $soapApiService;
+
+    /**
+     * Creditor constructor.
+     * @param SoapApi $soapApiService
+     */
+    public function __construct(SoapApiService $soapApiService)
+    {
+        $this->soapApiService = $soapApiService;
+    }
+}
