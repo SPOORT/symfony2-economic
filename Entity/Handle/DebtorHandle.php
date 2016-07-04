9b1868689e25f65a0c21b47ebef51a886285cbe7
Rune Mønnike <rm@gospoort.com>
2016-07-04 09:32:43 +0200

    Added constructor to DebtorHandle to construct from similar object.

--- Entity/Handle/DebtorHandle.php
+++ Entity/Handle/DebtorHandle.php
@@ -10,6 +10,21 @@ class DebtorHandle
     private $number;
 
     /**
+     * DebtorHandle constructor.
+     * Create a new handle from an object with matching properties.
+     * @param $source
+     */
+    public function __construct($source)
+    {
+        if (is_object($source) && isset($source->Number)) {
+            $this->setNumber($source->Number);
+        } else {
+
+            throw new \InvalidArgumentException(sprintf('Cannot construct %s from given source object: Does not contain property %s.', get_class(), 'Number'));
+        }
+    }
+
+    /**
      * @return int
      */
     public function getNumber()
